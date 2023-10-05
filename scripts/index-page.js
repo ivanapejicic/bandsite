f3b0444939259a55b0e89cf88a5b1cc3259253a1
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-10-05 17:22:43 -0400

    Displaying 3 default comments, adding new one on top, submit handler

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,8 +1,8 @@
 const comments = [
     {
-        name: 'Connor Walton',
-        date: "02/17/2021",
-        text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+        name: 'Miles Acosta',
+        date: "12/20/2020",
+        text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
     },
     {
         name: 'Emilie Beach',
@@ -10,13 +10,14 @@ const comments = [
         text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
     },
     {
-        name: 'Miles Acosta',
-        date: "12/20/2020",
-        text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+        name: 'Connor Walton',
+        date: "02/17/2021",
+        text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
     }
 ];
 
 const commentsCont = document.querySelector(".join__content-comments");
+const form = document.querySelector("form");
 
 function displayComment(comment) {
     const commentContainer = document.createElement("div");
@@ -31,7 +32,7 @@ function displayComment(comment) {
     const objTop = document.createElement("div");
     objTop.className = "comment__obj-top";
     const name = document.createElement("h3");
-    name.className = "commen__obj-top__name subheader-comments";
+    name.className = "comment__obj-top__name subheader-comments";
     name.textContent = comment.name;
     objTop.appendChild(name);
     const date = document.createElement("p");
@@ -52,6 +53,36 @@ function displayComment(comment) {
     commentsCont.appendChild(commentContainer);
 
 }
-for (const comment of comments) {
-    displayComment(comment);
+// displaying default comments
+for (let i=comments.length-1; i>-1; i--){
+    displayComment(comments[i]);
 }
+
+//hide form after submitting
+
+
+// handling form input
+function submitHandler(event){
+    event.preventDefault();
+    const nameElement = form.nameInput.value;
+    const commentElement = form.textInput.value;
+    const date = new Date(Date.now());
+    let formatDate = date.toLocaleDateString();
+    console.log(nameElement, commentElement);
+
+    let commentObject = {
+            name: nameElement,
+            date: formatDate,
+            text: commentElement
+    };
+    comments.push(commentObject);
+    commentsCont.innerText = '';
+
+    for (let i=comments.length-1; i>-1; i--){
+        displayComment(comments[i]);
+    }
+    form.nameInput.value= '';
+    form.textInput.value= '';
+}
+form.addEventListener("submit", submitHandler);
+
