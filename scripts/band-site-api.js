ad8aba738e0a7fab69fc6093784ccf91c4b3c899
Ivana Pejicic <ivana.pejicic001@mymdc.net>
2023-10-12 00:32:28 -0400

    getComments method

---
+++ scripts/band-site-api.js
@@ -0,0 +1,28 @@
+
+class BandSiteApi {
+
+    constructor(apiKey){
+        this.baseURL = ('https://project-1-api.herokuapp.com/');
+        this.apiKey = apiKey;
+    }
+    async getComments() {
+        const url = `${this.baseURL}comments?api_key=${this.apiKey}`;
+        
+        const response = await axios.get(url);
+        
+        const comments = response.data;
+        comments.sort(function(a,b){return b.timestamp - a.timestamp});
+        return comments;
+    }
+
+    async getShows() {
+        const url = `${this.baseURL}showdates?api_key=${this.apiKey}`;
+        console.log(url);
+        const response = await axios.get(url);
+        console.log(response);
+        const showArray = response.data;
+        console.log(showArray);
+        return showArray;
+        
+    }
+}
--- scripts/index-page.js
+++ scripts/index-page.js
@@ -1,23 +1,25 @@
-const comments = [
-    {
-        name: 'Miles Acosta',
-        date: "12/20/2020",
-        text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
-    },
-    {
-        name: 'Emilie Beach',
-        date: "01/09/2021",
-        text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
-    },
-    {
-        name: 'Connor Walton',
-        date: "02/17/2021",
-        text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
-    }
-];
+// const comments = [
+//     {
+//         name: 'Miles Acosta',
+//         date: "12/20/2020",
+//         text: "I can't stop listening. Every time I hear one of their songs - the vocals - it gives me goosebumps. Shivers straight down my spine. What a beautiful expression of creativity. Can't get enough."
+//     },
+//     {
+//         name: 'Emilie Beach',
+//         date: "01/09/2021",
+//         text: "I feel blessed to have seen them in person. What a show! They were just perfection. If there was one day of my life I could relive, this would be it. What an incredible day."
+//     },
+//     {
+//         name: 'Connor Walton',
+//         date: "02/17/2021",
+//         text: "This is art. This is inexplicable magic expressed in the purest way, everything that makes up this majestic work deserves reverence. Let us appreciate this for what it is and what it contains."
+//     }
+// ];
 
 const commentsCont = document.querySelector(".join__content-comments");
 const form = document.querySelector("form");
+const api_key = "11631f94-ded6-432e-9403-f17d7e61c05a";
+const backendComments = new BandSiteApi(api_key);
 
 function displayComment(comment) {
     const commentContainer = document.createElement("div");
@@ -36,13 +38,19 @@ function displayComment(comment) {
     name.textContent = comment.name;
     objTop.appendChild(name);
     const date = document.createElement("p");
+    const timestamp = comment.timestamp;
+    const formattedDate = (new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: '2-digit',
+        day: '2-digit'
+    })).format(new Date(timestamp));
     date.classList.add("comment__obj-top__date");
-    date.textContent = comment.date;
+    date.textContent = formattedDate;
     objTop.appendChild(date);
 
     const objBottom = document.createElement("p");
     objBottom.classList.add("comment__obj-bottom", "body-copy");
-    objBottom.textContent = comment.text;
+    objBottom.textContent = comment.comment;
 
     obj.appendChild(objTop);
     obj.appendChild(objBottom);
@@ -55,9 +63,20 @@ function displayComment(comment) {
 }
 
 // displaying default comments
-for (let i = comments.length - 1; i > -1; i--) {
-    displayComment(comments[i]);
+async function displayComments() {
+    try {
+        const comments = await backendComments.getComments();
+        console.log(comments);
+        // commentsCont.innerText = '';
+        for (const comment of comments) {
+            displayComment(comment);
+        }
+
+    } catch {
+        alert('cannot get data from API');
+    }
 }
+displayComments();
 
 // handling form input
 function submitHandler(event) {
@@ -68,11 +87,11 @@ function submitHandler(event) {
     const options = { year: 'numeric', month: '2-digit', day: '2-digit' };
     const formatDate = date.toLocaleDateString(undefined, options);
 
-    if (nameElement === ''){
+    if (nameElement === '') {
         alert("Please provide your name :-)");
         return;
     }
-    if (commentElement === ''){
+    if (commentElement === '') {
         alert("Don't you want to leave us a comment? :)");
         return;
     }
